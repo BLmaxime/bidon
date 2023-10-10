6470bed821d58258fbc2a6780313c7c4815fedf8
BLmaxime <benoitlegrandmaxime@gmail.com>
2023-10-10 17:25:02 +0200

    création du projet

--- src/main/java/sio/bidongithub/HelloController.java
+++ src/main/java/sio/bidongithub/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+int toto = 15;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
